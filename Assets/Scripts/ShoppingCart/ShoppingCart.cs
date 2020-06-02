ef8d79e079b987cadd3f88d3432e63990354c24f
Wx <g0546699m@gmail.com>
2020-06-02 23:49:50 +0800

    - Adding Cart - Renamed SOs to avoid confusion

---
+++ Assets/Scripts/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingCart : MonoBehaviour
+{
+    static public ShoppingCart Instance = null;
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void AddItem(GroceryItemObject newGroceryItem)
+    {
+
+    }
+    public void RemoveItem(GroceryItemObject itemToRemove)
+    {
+
+    }
+
+
+}

e675727b2d7342fc0cfca7e7c4c6f1f454d478a7
gideon <gideonnfrancis999@gmail.com>
2020-06-03 17:40:48 +0800

    populating home ui with stuffs

---
+++ Assets/Scripts/GroceryItem/ItemPrefab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemPrefab : MonoBehaviour
+{
+    [Header("Item Settings")]
+    [SerializeField] GameObject m_ItemName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Init(baseGroceryItemSO groceryItem)
+    {
+        if (groceryItem == null)
+            return;
+
+        // Set up the prefab
+        m_ItemName.GetComponent<TextMeshProUGUI>().text = groceryItem.GetItemName();
+
+    }
+}
---
+++ Assets/Scripts/HomeUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeUI : MonoBehaviour
+{
+    [Header("Home UI Settings")]
+    [SerializeField] GameObject UIItemPrefab;
+    [SerializeField] GameObject PopularItemReference;
+    [SerializeField] GameObject RecentItemReference;
+
+    List<baseGroceryItemSO> ListOfGroceryItems = new List<baseGroceryItemSO>();
+    int maxFoodItems = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxFoodItems = (int)baseGroceryItemSO.GROCERY_ID.TOTAL_GROCERY;
+
+        // Loop through all the items
+        // Add it into an internal list
+        // Just for temporarily populating stuff in the home screen
+        // Will be changed to use a database that stores items that are in stock
+        for (int index = 0; index < maxFoodItems; ++index)
+        {
+            ListOfGroceryItems.Add(GroceryItemDatabase.Instance.GetGroceryItem((baseGroceryItemSO.GROCERY_ID)index));
+        }
+
+        // For testing purposes, randomly select a few items from the grocery store as popular items
+        int startingNum = Random.Range(0, maxFoodItems - 5);
+        for (int i = startingNum; i < 5; ++i)
+        {
+            GameObject ItemPrefab = GameObject.Instantiate(UIItemPrefab, PopularItemReference.transform);
+
+            ItemPrefab.GetComponent<ItemPrefab>().Init(ListOfGroceryItems[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}

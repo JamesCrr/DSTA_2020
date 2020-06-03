38acf34e335e1dd4c037648c2db692770d7fca3d
Wx <g0546699m@gmail.com>
2020-06-03 15:49:25 +0800

    - Added Shopping cart View, still need to link to store screen

--- Assets/Scripts/GroceryItem/GroceryItemObject.cs
+++ Assets/Scripts/GroceryItem/GroceryItemObject.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GroceryItemObject : MonoBehaviour
 {
+    [Header("UI Ref")]
+    [SerializeField]
+    TextMeshProUGUI m_ItemNameText = null;
+    [SerializeField]
+    Image m_ItemImage = null;
+    [SerializeField]
+    TextMeshProUGUI m_ItemCounterText = null;
+
     baseGroceryItemSO m_SOReference = null;
+    int m_ItemCounter = 1;
 
     public void AssignSO(baseGroceryItemSO scriptableObject)
     {
         if (m_SOReference != null)
             return;
         m_SOReference = scriptableObject;
+
+        m_ItemCounter = 1;
+        UpdateTextCounter();
+        m_ItemNameText.text = m_SOReference.GetItemName();
+        m_ItemImage.sprite = m_SOReference.GetItemImage();
     }
     public baseGroceryItemSO GetAssignedSO() { return m_SOReference; }
 
 
+    public void IncreaseItemCountButtonPressed()
+    {
+        m_ItemCounter++;
+        UpdateTextCounter();
+    }
+    public void DecreaseItemCountButtonPressed()
+    {
+        m_ItemCounter--;
+        if(m_ItemCounter <= 0)
+        {
+            ShoppingCart.Instance.RemoveItem(m_SOReference.GetEnumID());
+            return;
+        }
+        UpdateTextCounter();
+    }
+    public void RemoveItemButtonPressed()
+    {
+        ShoppingCart.Instance.RemoveItem(m_SOReference.GetEnumID());
+    }
+    void UpdateTextCounter()
+    {
+        m_ItemCounterText.text = "x" + m_ItemCounter.ToString();
+    }
+
+    public int GetItemCount() { return m_ItemCounter; }
 }
--- Assets/Scripts/ShoppingCart/ShoppingCart.cs
+++ Assets/Scripts/ShoppingCart/ShoppingCart.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShoppingCart : MonoBehaviour
 {
@@ -15,28 +18,114 @@ public class ShoppingCart : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
+    Dictionary<baseGroceryItemSO.GROCERY_ID, GroceryItemObject> m_DictionaryCart = new Dictionary<baseGroceryItemSO.GROCERY_ID,GroceryItemObject>();
+    
+    [Header("UI Related")]
+    [SerializeField]
+    RectTransform m_CartUIParent = null;
+    bool m_CartViewActive = false;
+    [SerializeField]
+    Image m_CartButtonImage = null;
+    [SerializeField]
+    TextMeshProUGUI m_CartButtonText = null;
+    [Header("Display Items")]
+    [SerializeField]
+    Transform m_CartDisplayItemParent = null;
     [SerializeField]
     GroceryItemObject m_GroceryItemPrefab = null;
+    [Header("Total Price")]
+    [SerializeField]
+    TextMeshProUGUI m_TotalPriceText = null;
 
-    Dictionary<baseGroceryItemSO.GROCERY_ID, List<GroceryItemObject>> m_DictionaryCart = new Dictionary<baseGroceryItemSO.GROCERY_ID, List<GroceryItemObject>>();
-
-
-    public void AddItem(baseGroceryItemSO newGrocerySO)
+    private void Start()
     {
+        RecalculateScrollHeight();
+        RecalculatePrice();
+    }
+
+    //private void Update()
+    //{
+    //    if(Input.GetKeyUp(KeyCode.W))
+    //    {
+    //        AddItem(baseGroceryItemSO.GROCERY_ID.GI_MILK);
+    //    }
+    //    else if (Input.GetKeyUp(KeyCode.E))
+    //    {
+    //        RemoveItem(baseGroceryItemSO.GROCERY_ID.GI_MILK);
+    //    }
+    //}
+
+    public void AddItem(baseGroceryItemSO.GROCERY_ID itemID)
+    {
+        if (m_DictionaryCart.ContainsKey(itemID))
+        {
+            m_DictionaryCart[itemID].IncreaseItemCountButtonPressed();
+            // Price Recalculate
+            RecalculatePrice();
+            return;
+        }
+        // New Entry
         GroceryItemObject newItem = Instantiate(m_GroceryItemPrefab);
-        newItem.AssignSO(newGrocerySO);
-        if (!m_DictionaryCart.ContainsKey(newGrocerySO.GetEnumID()))
-            m_DictionaryCart.Add(newGrocerySO.GetEnumID(), new List<GroceryItemObject>());
-        m_DictionaryCart[newGrocerySO.GetEnumID()].Add(newItem);
+        newItem.AssignSO(GroceryItemDatabase.Instance.GetGroceryItem(itemID));
+        m_DictionaryCart.Add(itemID, newItem);
+        // Parent new Entry under for Vertical Layout
+        newItem.transform.parent = m_CartDisplayItemParent.transform;
+        // Scroll Height Recalculate
+        RecalculateScrollHeight();
+        // Price Recalculate
+        RecalculatePrice();
     }
-    public void RemoveItem(baseGroceryItemSO GrocerySO)
+    public void RemoveItem(baseGroceryItemSO.GROCERY_ID itemID)
     {
-        if (!m_DictionaryCart.ContainsKey(GrocerySO.GetEnumID()))
+        if (!m_DictionaryCart.ContainsKey(itemID))
             return;
-        if (m_DictionaryCart[GrocerySO.GetEnumID()].Count < 1)
-            return;
-        m_DictionaryCart[GrocerySO.GetEnumID()].RemoveAt(0);
+        // Destroy Object
+        GroceryItemObject itemToRemove = m_DictionaryCart[itemID];
+        itemToRemove.transform.parent = null;
+        Destroy(itemToRemove.gameObject);
+        m_DictionaryCart.Remove(itemID);
+        // Scroll Height Recalculate
+        RecalculateScrollHeight();
+        // Price Recalculate
+        RecalculatePrice();
     }
 
-    public Dictionary<baseGroceryItemSO.GROCERY_ID, List<GroceryItemObject>> GetCart() { return m_DictionaryCart; }
+    void RecalculatePrice()
+    {
+        float totalPrice = 0f;
+        foreach(KeyValuePair<baseGroceryItemSO.GROCERY_ID, GroceryItemObject> entry in m_DictionaryCart)
+        {
+            totalPrice += entry.Value.GetAssignedSO().GetItemPrice() * entry.Value.GetItemCount();
+        }
+        m_TotalPriceText.text = "$" + totalPrice.ToString();
+    }
+    void RecalculateScrollHeight()
+    {
+        Vector2 size = m_CartDisplayItemParent.GetComponent<RectTransform>().sizeDelta;
+        size.y = 138 * m_CartDisplayItemParent.childCount;
+        m_CartDisplayItemParent.GetComponent<RectTransform>().sizeDelta = size;
+    }
+
+    #region UI Related Functions
+    public void ToggleCartView()
+    {
+        m_CartViewActive = !m_CartViewActive;
+        if(m_CartViewActive)
+        {
+            m_CartUIParent.DOAnchorPosY(-58, 0.5f, true);
+            m_CartButtonImage.DOColor(Color.green, 0.4f);
+            m_CartButtonText.text = "Continue Shopping";
+        }
+        else
+        {
+            m_CartUIParent.DOAnchorPosY(-1184, 0.5f, true);
+            m_CartButtonImage.color = Color.white;
+            m_CartButtonText.text = "View Cart";
+        }
+    }
+
+    #endregion
+
+
+    public Dictionary<baseGroceryItemSO.GROCERY_ID, GroceryItemObject> GetCart() { return m_DictionaryCart; }
 }

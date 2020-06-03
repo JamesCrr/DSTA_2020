180aae903109082f42e97862e44334f8dce78b7d
Angie <173123N@mymail.nyp.edu.sg>
2020-06-03 21:26:21 +0800

    - dropdown prefab - Searching and being able to spawn the stuff in the list

---
+++ Assets/Scripts/UI/DropDownGroceryContent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DropDownGroceryContent : MonoBehaviour
+{
+    public TextMeshProUGUI m_GorceryName;
+    public Image m_GroceryImage;
+
+    baseGroceryItemSO.GROCERY_ID m_GroceryID;
+
+    public void Init(baseGroceryItemSO.GROCERY_ID groceryID)
+    {
+        m_GroceryID = groceryID;
+
+        baseGroceryItemSO groceryInfo = GroceryItemDatabase.Instance.GetGroceryItem(m_GroceryID);
+        m_GroceryImage.sprite = groceryInfo.GetItemImage();
+        m_GorceryName.text = groceryInfo.GetItemName();
+    }
+
+    public void Click()
+    {
+        //go to the correct page
+    }
+}
---
+++ Assets/Scripts/UI/DropDownOption.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DropDownOption : MonoBehaviour
+{
+    public GameObject m_DropDownContentPanel;
+    public TextMeshProUGUI m_TitleText;
+
+    public baseGroceryItemSO.CATEGORY m_Category;
+
+    public void Init(baseGroceryItemSO.CATEGORY category)
+    {
+        m_TitleText.text = category.ToString();
+        m_Category = category;
+
+        m_DropDownContentPanel.SetActive(false);
+    }
+
+    public void AddContentToPanel(GameObject newContent, baseGroceryItemSO.GROCERY_ID groceryID)
+    {
+        newContent.transform.SetParent(m_DropDownContentPanel.transform);
+
+        //init content stuff here
+        DropDownGroceryContent dropDownGroceryContent = newContent.GetComponent<DropDownGroceryContent>();
+        if (dropDownGroceryContent == null)
+            return;
+
+        dropDownGroceryContent.Init(groceryID);
+    }
+
+    public void RemoveAllContentToPanel()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    public void DropDownSwitch()
+    {
+        m_DropDownContentPanel.SetActive(!m_DropDownContentPanel.activeSelf);
+    }
+}
